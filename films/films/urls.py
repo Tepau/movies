62c9b0d7ec392bb5fc7501f47d8f5f0bc93c9bce
mle <malaury.lemaitresalmon@sciencespo.fr>
2023-01-11 15:27:14 +0100

    j2

--- films/films/urls.py
+++ films/films/urls.py
@@ -21,7 +21,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('listing/', views.MovieListView.as_view())
+    path('listing/', views.MovieListView.as_view(), name='movie-list')
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- films/listing/views.py
+++ films/listing/views.py
@@ -9,3 +9,16 @@ def index(request):
 class MovieListView(ListView):
     model = Movie
     template_name = 'movie_list.html'
+
+    def get_queryset(self):
+        year = self.request.GET.get('year')
+        qs = super().get_queryset()
+        if year is not None:
+            return qs.filter(date__year=year)
+        return qs
+    
+    def get_context_data(self, **kwargs):
+        data = super().get_context_data(**kwargs)
+        years = [movie.date.year for movie in Movie.objects.all()]
+        data['years'] = list(set(years))
+        return data
